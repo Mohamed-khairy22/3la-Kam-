55567b08af09dd7333bc368e1ea120c5b990a04d
mohamed-khairy <mohamedkhairy063036@gmail.com>
2024-09-05 05:12:02 +0300

    using viewChild

--- 3laKamFullProject/src/app/Component/Order/order-master/order-master.component.ts
+++ 3laKamFullProject/src/app/Component/Order/order-master/order-master.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, ViewChild, viewChild } from '@angular/core';
 import { ProductListComponent } from "../product-list/product-list.component";
 import { Icategory } from '../../../Model/icategory';
 import { FormsModule } from '@angular/forms';
@@ -12,16 +12,21 @@ import { USDtoEGPPipe } from '../../../Pipes/usdto-egp.pipe';
   templateUrl: './order-master.component.html',
   styleUrl: './order-master.component.scss'
 })
-export class OrderMasterComponent {
+export class OrderMasterComponent implements AfterViewInit{
   catList:Icategory[];
   selectedCatID:number=0;
   orderTotalPrice:number=0;
+  @ViewChild("changeName") changeYourName!:ElementRef;
   constructor(){
     this.catList=[
       {id:1,name:'Laptop'},
       {id:2,name:'Taplet'},
       {id:3,name:'Mopile'}
     ]
+  }
+  ngAfterViewInit(): void {
+    this.changeYourName.nativeElement.value="Enter your name here";
+    this.changeYourName.nativeElement.style.border="2px solid red"
   }
   totalPrice(price:number)
   {
