31b47209a0ce575e847f04c67b269ed6422f4f92
mohamed-khairy <mohamedkhairy063036@gmail.com>
2024-09-05 09:30:42 +0300

    Enable editing in cart values

--- 3laKamFullProject/src/app/Component/Order/order-master/order-master.component.ts
+++ 3laKamFullProject/src/app/Component/Order/order-master/order-master.component.ts
@@ -5,57 +5,61 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { USDtoEGPPipe } from '../../../Pipes/usdto-egp.pipe';
 import { NewCart } from '../../../ViewModel/new-cart';
-import { Iproduct } from '../../../Model/iproduct';
 
 @Component({
   selector: 'app-order-master',
   standalone: true,
-  imports: [ProductListComponent,FormsModule,CommonModule,USDtoEGPPipe],
+  imports: [ProductListComponent, FormsModule, CommonModule, USDtoEGPPipe],
   templateUrl: './order-master.component.html',
   styleUrl: './order-master.component.scss'
 })
-export class OrderMasterComponent implements AfterViewInit{
-  catList:Icategory[];
-  myCart:NewCart[]=[];
-  selectedCatID:number=0;
-  orderTotalPrice:number=0;
-  @ViewChild("changeName") changeYourName!:ElementRef;
+export class OrderMasterComponent implements AfterViewInit {
+  catList: Icategory[];
+  myCart: NewCart[] = [];
+  selectedCatID: number = 0;
+  orderTotalPrice: number = 0;
+  myNewCount :number=0;
+  @ViewChild("changeName") changeYourName!: ElementRef;
 
-  constructor(){
-    this.catList=[
-      {id:1,name:'Laptop'},
-      {id:2,name:'Taplet'},
-      {id:3,name:'Mopile'}
-    ]     
-    
+  constructor() {
+    this.catList = [
+      { id: 1, name: 'Laptop' },
+      { id: 2, name: 'Taplet' },
+      { id: 3, name: 'Mopile' }
+    ]
+
   }
   ngAfterViewInit(): void {
-    this.changeYourName.nativeElement.value="Enter your name here";
-    this.changeYourName.nativeElement.style.border="2px solid red"
+    this.changeYourName.nativeElement.value = "Enter your name here";
+    this.changeYourName.nativeElement.style.border = "2px solid red"
   }
-  addToCart(prd:NewCart)
-  {    let x:number=-1;
+  addToCart(prd: NewCart) {
+    let x: number = -1;
     for (let index = 0; index < this.myCart.length; index++) {
-      if(this.myCart[index].id==prd.id)
-          x=index;
+      if (this.myCart[index].id == prd.id)
+        x = index;
     }
-    if(x!=-1)
-      {
-        this.myCart[x].count+=prd.count;
-        this.orderTotalPrice+=(prd.count * prd.price)
-      }
-    else{
+    if (x != -1) {
+      this.myCart[x].count += prd.count;
+      this.orderTotalPrice += (prd.count * prd.price)
+    }
+    else {
       this.myCart.push(prd)
-      this.orderTotalPrice+=(prd.count * prd.price)
+      this.orderTotalPrice += (prd.count * prd.price)
 
     }
   }
 
-  deleteFromCart(item:NewCart)
+  deleteFromCart(item: NewCart) {
+    this.myCart = this.myCart.filter(i => i.id != item.id)
+    this.orderTotalPrice -= (item.count * item.price)
+  }
+  changeCount(i:number,count:any)
   {
-    this.myCart=this.myCart.filter(i=>i.id!=item.id)
-    this.orderTotalPrice-=(item.count * item.price)
-
+    this.myNewCount =count;
+    this.orderTotalPrice+=(this.myNewCount - this.myCart[i].count)*this.myCart[i].price
+    this.myCart[i].count=this.myNewCount;
+    
   }
 
 }
