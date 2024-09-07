07238a0b7828fc61a5aba4b6aac37ec27ff3287c
mohamed-khairy <mohamedkhairy063036@gmail.com>
2024-09-07 08:38:05 +0300

    intializing routes in app.routes

--- 3laKamFullProject/src/app/app.routes.ts
+++ 3laKamFullProject/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Component/home/home.component';
+import { ProductListComponent } from './Component/Order/product-list/product-list.component';
+import { OrderMasterComponent } from './Component/Order/order-master/order-master.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home',component:HomeComponent},
+    {path:'products',component:ProductListComponent},
+    {path:'order',component:OrderMasterComponent}
+
+
+
+];
