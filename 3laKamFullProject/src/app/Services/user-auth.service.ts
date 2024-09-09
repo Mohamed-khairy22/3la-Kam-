075a4dd2f0d7f46363080885024e3d0b813344de
mohamed-khairy <mohamedkhairy063036@gmail.com>
2024-09-10 00:05:44 +0300

    create functions login, logout and isUserLogged in  user authentication service

--- 3laKamFullProject/src/app/Services/user-auth.service.ts
+++ 3laKamFullProject/src/app/Services/user-auth.service.ts
@@ -6,4 +6,22 @@ import { Injectable } from '@angular/core';
 export class UserAuthService {
 
   constructor() { }
+  login(userName:string,password:string)
+  {
+    //call login API, and get access token
+    let usrToken="123456789";
+    localStorage.setItem("token",usrToken);
+
+
+  }
+  logout()
+  {
+    localStorage.removeItem("token")
+
+  }
+  get isUserLogged():boolean
+  {
+    return (localStorage.getItem("token"))?true:false;
+
+  }
 }
