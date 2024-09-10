9648b241ce468be92d869a786807adaeec6efdb7
mohamed-khairy <mohamedkhairy063036@gmail.com>
2024-09-10 06:13:00 +0300

    create login form and handel it in ts file

--- 3laKamFullProject/src/app/Component/login/login.component.ts
+++ 3laKamFullProject/src/app/Component/login/login.component.ts
@@ -1,29 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { UserAuthService } from '../../Services/user-auth.service';
+import { CommonModule, Location } from '@angular/common';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  constructor(private userAuth:UserAuthService ){
+  constructor(private userAuth:UserAuthService 
+            , private location: Location
+  ){
 
   }
 
-  login(){
-    this.userAuth.login("userName","password");
-
-  }
-  logout()
-  {
-    this.userAuth.logout();
-
+  login(userName:string,pass:string){
+    this.userAuth.login(userName,pass);   
+    this.location.back()
   }
   isLogged():boolean
   {
    return this.userAuth.isUserLogged;
   }
+  
 }
